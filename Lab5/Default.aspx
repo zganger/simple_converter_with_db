61d2987498470f63301b962705915b40db6a60dc
Zack Ganger <zganger@bu.edu>
2016-03-27 14:29:33 -0400

    add files

---
+++ Lab5/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Unit Converter</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <center><table style="width: 677px"><tr><td colspan="2">
+    <h1 style="text-align: center;">Unit Conversion</h1></tr>
+        <tr>
+            <td colspan="2"><asp:TextBox ID="num1" runat="server"></asp:TextBox><asp:DropDownList ID="fromList" runat="server" DataSourceID="SqlDataSource1" DataTextField="Unit" DataValueField="Unit"></asp:DropDownList> 
+                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [Unit] FROM [Conversions]"></asp:SqlDataSource>
+                = <asp:TextBox ID="num2" runat="server" ReadOnly="True"></asp:TextBox><asp:DropDownList ID="toList" runat="server" DataSourceID="SqlDataSource1" DataTextField="Unit" DataValueField="Unit"></asp:DropDownList></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center; padding-top:25px;">
+                <asp:Button ID="Convert" runat="server" Text="Convert" OnClick="Convert_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center; padding-top:25px;" class="auto-style1"><asp:TextBox ID="ErrorBox" runat="server" style="text-align:center" Visible="False" ReadOnly="True" Width="346px"></asp:TextBox></td>
+        </tr>
+    </table></center>
+    </form>
+</body>
+</html>
---
+++ Lab5/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected double[] conv = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private const string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Lab5DB.mdf;Integrated Security = True";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(connString);
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM Conversions", conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+        int i = 0;
+        while (reader.Read())
+        {
+            double f = (double)reader["Factor"];
+            conv[i] = f;
+            i++;
+        }
+        conn.Close();
+    }
+
+    protected void Convert_Click(object sender, EventArgs e)
+    {
+        double innum;
+        ErrorBox.Visible = false;
+        int inindex = fromList.SelectedIndex;
+        int outindex = toList.SelectedIndex;
+        if (inindex == outindex)
+        {
+            num1.Text = "";
+            ErrorBox.Text = "You're converting to the same unit! Try again.";
+            ErrorBox.Visible = true;
+        }
+        else
+        {
+            if (Double.TryParse(num1.Text, out innum))
+            {
+                ErrorBox.Visible = false;
+                num2.Text = ((innum * conv[inindex]) / conv[outindex]).ToString();
+            }
+            else
+            {
+                num1.Text = "";
+                ErrorBox.Text = "Please enter a valid number!";
+                ErrorBox.Visible = true;
+            }
+        }
+    }
+}
